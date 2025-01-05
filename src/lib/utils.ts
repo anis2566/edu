9b94ad8e6ebc8ab9921c1651c2c8c19cf7c9fe3b
shams256 <shamspublication57@gmail.com>
2025-01-05 20:43:20 +0600

    fafa

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,9 +13,9 @@ export const formatPrice = (price: number) => {
 };
 
 export function secondsToHMS(seconds: number) {
-  let hours = Math.floor(seconds / 3600);
-  let minutes = Math.floor((seconds % 3600) / 60);
-  let remainingSeconds = seconds % 60;
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
 
   return `${hours}h ${minutes}m ${remainingSeconds}s`;
 }
